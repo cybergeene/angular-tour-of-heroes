d7dc89beaf0b0868c4386b50bacdee79d0c7a24d
mednov <e.mednov@mail.ru>
2018-07-19 02:20:57 +0300

    correction heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import {Hero} from "./hero";
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Мистер Найс'},
+  { id: 12, name: 'Нарко'},
+  { id: 13, name: 'Бомбасто'},
+  { id: 14, name: 'Целеристас'},
+  { id: 15, name: 'Магнето'},
+  { id: 16, name: 'Человек-резина'},
+  { id: 17, name: 'Динамо'},
+  { id: 18, name: 'Доктор АйКью'},
+  { id: 19, name: 'Магма'},
+  { id: 20, name: 'Торнадо'}
+]
